d79910e0c1fc56e6776b9a64aae02e223e39aa9e
shivamkrs89 <bigbinary@BigBinarys-MacBook-Pro-2.local>
2022-09-03 22:22:59 +0530

    Created Toastr component and added Axios interceptors

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
-import { setAuthHeaders } from "apis/axios";
+import { registerIntercepts, setAuthHeaders } from "apis/axios";
 import { initializeLogger } from "common/logger";
 import Dashboard from "components/Dashboard";
 import PageLoader from "components/PageLoader";
@@ -10,14 +11,11 @@ import CreateTask from "components/Tasks/Create";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
-  // previous code if any
 
   useEffect(() => {
-    /*eslint no-undef: "off"*/
     initializeLogger();
+    registerIntercepts();
     setAuthHeaders(setLoading);
-    // logger.info("Never use console.log");
-    // logger.error("Never use console.error");
   }, []);
 
   if (loading) {
@@ -30,10 +28,8 @@ const App = () => {
 
   return (
     <Router>
+      <ToastContainer />
       <Switch>
-        <Route exact path="/" render={() => <div>Home</div>} />
-        <Route exact path="/about" render={() => <div>About</div>} />
-        <Route exact component={Dashboard} path="/dashboard" />
         <Route exact component={CreateTask} path="/tasks/create" />
         <Route exact component={Dashboard} path="/dashboard" />
       </Switch>
